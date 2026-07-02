0b3c9dca50070b1349a171712075aa0462e265c1
lir-07 <lirveseli08@gmail.com>
2026-07-02 14:44:14 +0200

    Update

--- javascript.js
+++ javascript.js
@@ -15,7 +15,76 @@ function getComputerChoice(){
 
 function getHumanChoice(){
     let choice = prompt("What is your choice(rock, paper, scissors)?");
+    return choice;
 }
 
 let humanScore = 0;
 let computerScore = 0;
+let win = "false";
+
+function playRound(humanChoice, computerChoice){
+    let humanch = humanChoice.toLowerCase();
+
+    if (humanch === "rock"){
+        if(computerChoice === "rock"){
+            win = "tie";
+        }
+        if(computerChoice === "scissors"){
+            win = "true";
+        }
+        if(computerChoice === "paper"){
+            win = "false";
+        }
+    }
+
+    if (humanch === "paper"){
+        if(computerChoice === "rock"){
+            win = "true";
+        }
+        if(computerChoice === "scissors"){
+            win = "false";
+        }
+        if(computerChoice === "paper"){
+            win = "tie";
+        }
+    }
+
+    if (humanch === "scissors"){
+        if(computerChoice === "rock"){
+            win = "false";
+        }
+        if(computerChoice === "scissors"){
+            win = "tie";
+        }
+        if(computerChoice === "paper"){
+            win = "true";
+        }
+    }
+
+    if(win === "false"){
+        console.log("You lose! " + computerChoice + " beats " + humanChoice + ".");
+        computerScore++;
+    }
+
+    if(win === "true"){
+            console.log("You win! " + humanChoice + " beats " + computerChoice + ".");
+        humanScore++;
+    }
+
+    if(win === "tie"){
+        console.log("Tie! Both picked the same thing.")
+    }
+
+}
+
+function playGame(){
+
+    for (let i = 0; i < 5; i++){
+        let humanSelection = getHumanChoice();
+        let computerSelection = getComputerChoice();
+
+        playRound(humanSelection, computerSelection);
+    }
+}
+
+playGame();
